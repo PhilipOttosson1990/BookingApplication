7f961be09180cc89afda7fc7a9c78d7af9458fa9
Philipottosson <77959523+Philipottosson@users.noreply.github.com>
2023-08-21 20:43:05 +0200

    Add project files.

---
+++ PhilipsBokningsApplication/Controllers/BookingController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhilipsBokningsApplication.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PhilipsBookingApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> GetAllBookings()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string GetBooking(Guid id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] BookningModel BookingValue)
+        {
+
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] BookningModel BookingValue)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PhilipsBokningsApplication/Models/BookningModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PhilipsBokningsApplication.Models
+{
+    public class BookningModel
+    {
+        public User User { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
